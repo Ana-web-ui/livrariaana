121f34ad19796954e944f3e75f9d3111268950c6
ana leite <anacarolinaleite1401@gmail.com>
2024-10-28 09:50:18 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,30 +1,40 @@
-fetch('http://localhost:3000/livros')
-    .then(resposta => resposta.json())
-    .then(resposta =>{
-        const lista = document.querySelector('#list');
-        console.log(lista);
-        
-        
-        
-        let result = '';
-        resposta.forEach(el =>{
-            result += `
-         <li class="flex justify-between gap-x-6 py-5">
-              <div class="flex min-w-0 gap-x-4">
-                <div class="min-w-0 flex-auto">
-                  <p class="text-sm font-semibold leading-6 text-gray-900">${el.titulo}</p>
-                  <p class="mt-1 truncate text-xs leading-5 text-green-500">${el.status}</p>
-                </div>
-              </div>
-              <div class="hidden shrink-0 sm:flex sm:flex-col sm:items-end">
-                <p class="text-sm leading-6 text-gray-900">${el.preco}</p>
-                
-              </div>
-            </li>
-        
-        `
-        })
-        
-        lista.innerHTML = result;
-    })
 
+const express = require('express');
+const app = express();
+const PORT = 3000;
+const cors = require('cors'); // Importe o middleware CORS
+
+app.use(cors());
+app.use(express.json());
+
+const livros = [
+    {
+        titulo: 'Código Limpo',
+        preco: 50,
+        qtdPagina:300,
+        status: true
+    },
+
+    {
+        titulo: 'Use a Cabeça!',
+        preco:80,
+        qtdPagina: 500,
+        status: true
+    },
+
+    {
+        titulo: 'Domain Drive Design',
+        preco: 60,
+        qtdPagina: 200,
+        status: false
+    }
+]
+
+
+app.get('/livros', (req, res) => {
+    res.json(livros);
+});
+
+app.listen(PORT, () => {
+    console.log(`Servidor rodando em http://localhost:${PORT}`);
+});
